b29f7b1ad64958c7e61cd7c20064e4f34ebc7348
Aditi Kumari <aditi12395@gmail.com>
2019-04-24 17:07:26 -0700

    Order API added

--- orders-rest-api/src/main.go
+++ orders-rest-api/src/main.go
@@ -1,40 +1,16 @@
 package main
 
-// import (
-// 	"os"
-// )./
+import (
+	"os"
+)
 
-// func main() {
+func main() {
 
-// 	port := os.Getenv("PORT")
-// 	if len(port) == 0 {
-// 		port = "8000"
-// 	}
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8000"
+	}
 
-// 	server := NewServer()
-// 	server.Run(":" + port)
-// }
-
-// func main() {
-// 	// We need this object to establish a session to our MongoDB.
-// 	mongoDBDialInfo := &mgo.DialInfo{
-// 		Addrs:    []string{MongoDBHosts},
-// 		Timeout:  20 * time.Second,
-// 		Database: AuthDatabase,
-// 		Username: AuthUserName,
-// 		Password: AuthPassword,
-// 	}
-
-// 	// Create a session which maintains a pool of socket connections
-// 	// to our MongoDB.
-// 	mongoSession, err := mgo.DialWithInfo(mongoDBDialInfo)
-// 	if err != nil {
-// 		log.Fatalf("CreateSession: %s\n", err)
-// 	} else {
-// 		fmt.Printf("successful")
-// 	}
-// 	mongoSession.SetMode(mgo.Monotonic, true)
-
-// 	r := mux.NewRouter()
-// 	http.ListenAndServe(":8090", r)
-// }
+	server := NewServer()
+	server.Run(":" + port)
+}
